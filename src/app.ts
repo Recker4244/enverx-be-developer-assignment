ba6d40fc36249d1a2b7829c902081bb126852401
Recker4244 <ashutoshsenapati2311@gmail.com>
2023-08-07 19:19:54 +0530

    chore: initial setup

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+const express = require('express');
+const {router} = require('./routes/routes');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
